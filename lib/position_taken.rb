bf3f30aa26611250833c4b044ae1725ae10ceca8
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-17 17:48:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-   board[index] 
-   puts board.inspect
-   puts index
-   puts board[index]
+   position = board[index] 
+   if position == " "
+     return FALSE
+   end
+   
 end
